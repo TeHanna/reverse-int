ca23a60dbccce8e2378871c4cd31397cfe90b14d
TeHanna <68251610+TeHanna@users.noreply.github.com>
2021-08-19 03:29:35 +0300

    feat:  change index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+  if (n > 0) {
+    let revStr = '';
+    for (let i = 1; i < n.toString().length; i++) {
+      revStr += Math.floor((n/Math.pow(10, i)))%10;
+    }
+    return ((`${n%10}` + revStr)*1);
+  } else {
+    n = n * (-1);
+    let revStr = '';
+    for (let i = 1; i < n.toString().length; i++) {
+      revStr += Math.floor((n/Math.pow(10, i)))%10;
+    }
+    return ((`${n%10}` + revStr)*1);
+  }
 }
